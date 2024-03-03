78a54326b7aa12ba09b91c7062ee385c4cf3bd98
WannDEV <matthiaswannjensen@gmail.com>
2024-03-03 22:21:32 +0100

    Added example env file

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,12 +1,15 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
+import dotenv from "dotenv";
+dotenv.config();
+
 // Lav en PostgreSQL pool
 const pool = new Pool({
   user: "social_media_api",
   host: "localhost",
   database: "social_media",
-  password: "demo123",
+  password: process.env.DB_PASSWORD,
   port: 5432, // Standard PostgreSQL port
 });
 
